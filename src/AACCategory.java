e55dd347b0e99a22addf446ba2876324e1ecf53d
Tim Yu <yuyu.tim@gmail.com>
2024-03-06 17:11:32 -0600

    Implemented all methods of AACCategory except getImages

--- src/AACCategory.java
+++ src/AACCategory.java
@@ -1,4 +1,7 @@
 import structures.AssociativeArray;
+import structures.KeyNotFoundException;
+import structures.NullKeyException;
+import java.io.PrintWriter;
 
 /**
  * A mapping between image location strings and image description strings,
@@ -9,6 +12,7 @@ import structures.AssociativeArray;
 public class AACCategory {
   String name;
   private AssociativeArray<String, String> imageMappings;
+  private PrintWriter err;
 
   /**
    * Creates a new empty category with the given name
@@ -16,40 +20,49 @@ public class AACCategory {
   public AACCategory(String name) {
     this.name = name;
     this.imageMappings = new AssociativeArray<String, String>();
+    this.err = new PrintWriter(System.err, true);
   }
 
   /**
    * Adds the mapping of the imageLoc to the text to the category.
    */
   public void addItem(String imageLoc, String text) {
-    return;
+    try {
+      this.imageMappings.set(imageLoc, text);
+    } catch (NullKeyException e) {
+      err.println("Image path cannot be null");
+    }
   }
 
   /**
    * Returns the name of the category
    */
   public String getCategory() {
-    return ""; // STUB
+    return name;
   }
 
   /**
    * Returns the text associated with the given image loc in this category.
    */
   public String getText(String imageLoc) {
-    return ""; // STUB
+    try {
+      return imageMappings.get(imageLoc);
+    } catch (KeyNotFoundException e) {
+      return "";
+    }
   }
 
   /**
    * Determines if the provided images is stored in the category
    */
   public boolean hasImage(String imageLoc) {
-    return false; //
+    return imageMappings.hasKey(imageLoc);
   }
 
   /**
    * Return an array of all the images in the category
    */
   public String[] getImages() {
-    return new String[]{};
+    return new String[]{}; // STUB
   }
 }
