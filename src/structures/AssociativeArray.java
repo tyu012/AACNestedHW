7be5692cecc32a2e39f83e2bc06dfa04c77dc6f9
Tim Yu <yuyu.tim@gmail.com>
2024-04-14 18:48:11 -0500

    Use == instead of equals to check for null

--- src/structures/AssociativeArray.java
+++ src/structures/AssociativeArray.java
@@ -97,7 +97,7 @@ public class AssociativeArray<K, V> {
    * get(key) will return value.
    */
   public void set(K key, V value) throws NullKeyException {
-    if (key.equals(null)) {
+    if (key == null) {
       throw new NullKeyException();
     }
 
@@ -132,7 +132,7 @@ public class AssociativeArray<K, V> {
    * return false for the null key.
    */
   public boolean hasKey(K key) {
-    if (key.equals(null)) {
+    if (key == null) {
       return false;
     }
     try {
@@ -184,7 +184,7 @@ public class AssociativeArray<K, V> {
       throw new KeyNotFoundException("This associative array is empty");
     }
   }
-
+  
   // +-----------------+---------------------------------------------
   // | Private Methods |
   // +-----------------+
@@ -202,7 +202,7 @@ public class AssociativeArray<K, V> {
    * An entry with key must be stored in [0, size) to be found.
    */
   int find(K key) throws KeyNotFoundException {
-    if (key.equals(null)) {
+    if (key == null) {
       throw new KeyNotFoundException();
     }
     for (int i = 0; i < size(); i++) {
